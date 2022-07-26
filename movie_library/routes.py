6136b2a5404b29d7df0cb5e1abe1bd7ccc5680f9
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-07-26 03:48:09 -0300

    gitignore adjust, movies style and database config

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -14,9 +14,12 @@ pages = Blueprint(
 
 @pages.route("/")
 def index():
+    movie_data = current_app.db.movie.find({})
+    movies = [Movie(**movie) for movie in movie_data]
     return render_template(
         "index.html",
-        title="Movies Watchlist",
+        title="Movies List",
+        movies_data=movies,
     )
 
 
