a4e11411c21f272c32a5a54b9d3d41a6d06b9e54
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-07-26 03:09:06 -0300

    dark/light theme add

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, session
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -11,3 +11,14 @@ def index():
         "index.html",
         title="Movies List",
     )
+
+
+@pages.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))
